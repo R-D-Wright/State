16e8136a6f9e75d4dc035ac352f17716a7644098
Drake Wright <rdrakewright@gmail.com>
2023-05-17 08:35:11 -0500

    Initial Commit

---
+++ State/StateApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateApp.swift
+//  State
+//
+//  Created by Drake Wright on 5/17/23.
+//
+
+import SwiftUI
+
+@main
+struct StateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
